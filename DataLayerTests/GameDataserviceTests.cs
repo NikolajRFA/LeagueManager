3d7afe326a6ae5bf69c0f5374d5b2c4d7d8f010f
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-01-18 23:02:24 +0100

    Added tests for GetGamesFromPlayer

---
+++ DataLayerTests/GameDataserviceTests.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.DataServices;
+
+namespace DataLayerTests;
+
+public class GameDataserviceTests
+{
+    [Fact]
+    public void GetGamesFromPlayer_Id1_Success()
+    {
+        var dataService = new GameDataService();
+        Assert.NotNull(dataService.GetGamesFromPlayer(1));
+    }
+
+    [Fact]
+    public void GetGamesFromPlayer_NonExistent_ReturnsNull()
+    {
+        var dataService = new GameDataService();
+        Assert.Null(dataService.GetGamesFromPlayer(-1));
+    }
+}
