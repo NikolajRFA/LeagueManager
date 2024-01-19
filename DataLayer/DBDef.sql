d9e82f7462df0d750aeee3be9fa61cf55d0834cb
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-01-19 14:57:18 +0100

    Removed is_active from member table

--- DataLayer/DBDef.sql
+++ DataLayer/DBDef.sql
@@ -42,8 +42,7 @@ CREATE TABLE member (
                         team_id INTEGER REFERENCES team(id),
                         role ROLE,
                         from_date DATE,
-                        to_date DATE,
-                        is_active BOOLEAN NOT NULL DEFAULT false
+                        to_date DATE
 );
 
 CREATE TABLE game (
@@ -83,17 +82,17 @@ INSERT INTO player (id, first_name, last_name, alias, age, gender, nationality, 
 INSERT INTO player (id, first_name, last_name, alias, age, gender, nationality, game_sense, team_fighting, dueling, jgl_pathing, wave_mgmt, farming) VALUES (10, 'Dobrivoje', 'Mišković', 'organiclion133', 25, 'Male', 'RS', 78, 91, 97, 60, 99, 94);
 
 
-INSERT INTO member (player_id, team_id, role, from_date, to_date, is_active) VALUES 
-(1,  1, 'top',     CURRENT_DATE, null, false),
-(2,  1, 'jungle',  CURRENT_DATE, null, false),
-(3,  1, 'mid',     CURRENT_DATE, null, false),
-(4,  1, 'bottom',  CURRENT_DATE, null, false),
-(5,  1, 'support', CURRENT_DATE, null, false),
-(6,  2, 'top',     CURRENT_DATE, null, false),
-(7,  2, 'jungle',  CURRENT_DATE, null, false),
-(8,  2, 'mid',     CURRENT_DATE, null, false),
-(9,  2, 'bottom',  CURRENT_DATE, null, false),
-(10, 2, 'support', CURRENT_DATE, null, false);
+INSERT INTO member (player_id, team_id, role, from_date, to_date) VALUES 
+(1,  1, 'top',     CURRENT_DATE, null),
+(2,  1, 'jungle',  CURRENT_DATE, null),
+(3,  1, 'mid',     CURRENT_DATE, null),
+(4,  1, 'bottom',  CURRENT_DATE, null),
+(5,  1, 'support', CURRENT_DATE, null),
+(6,  2, 'top',     CURRENT_DATE, null),
+(7,  2, 'jungle',  CURRENT_DATE, null),
+(8,  2, 'mid',     CURRENT_DATE, null),
+(9,  2, 'bottom',  CURRENT_DATE, null),
+(10, 2, 'support', CURRENT_DATE, null);
 
 INSERT INTO game (blue_side_id, red_side_id, winner_id, date) VALUES (1, 2, NULL, now());
 
