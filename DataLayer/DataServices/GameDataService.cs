53b64d52f71c03b0f6183084fca73687743d2513
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-01-19 16:59:22 +0100

    Started programming PlayGame method

--- DataLayer/DataServices/GameDataService.cs
+++ DataLayer/DataServices/GameDataService.cs
@@ -35,4 +35,38 @@ public class GameDataService
             .Include(x => x.Players)
             .FirstOrDefault(x => x.Id == gameId)?.Players;
     }
+
+    public Game PlayGame(int blueSideTeamId, int redSideTeamId, DateOnly gameDate)
+    {
+        var db = new Database();
+        var game = new Game
+        {
+            BlueSideId = blueSideTeamId,
+            RedSideId = redSideTeamId
+        };
+        var dbGame = db.Games.Add(game);
+
+        var gameTeamsActiveMembers = db.Members
+            .Where(x => (x.TeamId == blueSideTeamId || x.TeamId == redSideTeamId)
+                        && x.Role != null
+                        && x.FromDate < gameDate
+                        && x.ToDate > gameDate
+            ).ToList();
+
+        var participations = new List<Participation>();
+        foreach (var member in gameTeamsActiveMembers)
+        {
+            participations.Add(new Participation
+            {
+                GameId = dbGame.Entity.Id,
+                PlayerId = member.PlayerId,
+                Role = member.Role!,
+                TeamId = member.TeamId
+            });
+        }
+        
+        db.Participations.AddRange(participations);
+        
+        // TODO: Calculate who won
+    }
 }
--- DataLayer/Entities/Member.cs
+++ DataLayer/Entities/Member.cs
@@ -4,7 +4,7 @@ public class Member
 {
     public int PlayerId { get; set; }
     public int TeamId { get; set; }
-    public string Role { get; set; }
+    public string? Role { get; set; }
     public DateOnly FromDate { get; set; }
     public DateOnly ToDate { get; set; }
     public Player Player { get; set; }
