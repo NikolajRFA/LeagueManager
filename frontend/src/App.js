1fae4d39c66c4f0c8b3188ad9ae520473e992554
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-01-31 00:00:57 +0100

    Started making player page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home";
 import Player from "./Pages/Player";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/player" element={<Player/>}/>
             </Routes>
         </BrowserRouter>
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,32 @@
+import logo from "../logo.svg";
+import {Button} from "@mui/material";
+import Loading from "../Components/Loading";
+import {usePlayer} from "../BusinessLogic/usePlayer";
+
+export default function Home() {
+    const playerData = usePlayer();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button variant="contained" onClick={() => alert('Button was clicked!')}>Contained</Button>
+                <Loading isLoading={Object.keys(playerData).length === 0}>
+                    <Button variant="contained"
+                            onClick={() => alert(`Player API url is: ${playerData.url}`)}>{playerData.firstName} '{playerData.alias}' {playerData.lastName}: {playerData.age}</Button>
+                </Loading>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
+}
