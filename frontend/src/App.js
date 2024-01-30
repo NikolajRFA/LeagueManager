af8dd7fe5f17b11103def4e3cc1ef27d565d2d8d
NikolajRFA <nrfadk@gmail.com>
2024-01-30 19:16:06 +0100

    Created Loading.jsx and added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,34 +3,34 @@ import './App.css';
 import {Button} from "@mui/material";
 import {usePlayer} from "./BusinessLogic/usePlayer";
 import {useEffect} from "react";
+import Loading from "./Components/Loading";
 
 function App() {
-  const playerData = usePlayer();
+    const playerData = usePlayer();
 
-  useEffect(() => {
-
-  }, [playerData]);
-
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button variant="contained" onClick={() => alert('Button was clicked!')}>Contained</Button>
-          <Button variant="contained" onClick={() => alert(`Player API url is: ${playerData.url}`)}>{playerData.firstName} '{playerData.alias}' {playerData.lastName}: {playerData.age}</Button>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button variant="contained" onClick={() => alert('Button was clicked!')}>Contained</Button>
+                <Loading isLoading={Object.keys(playerData).length === 0}>
+                    <Button variant="contained"
+                            onClick={() => alert(`Player API url is: ${playerData.url}`)}>{playerData.firstName} '{playerData.alias}' {playerData.lastName}: {playerData.age}</Button>
+                </Loading>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/BusinessLogic/usePlayer.js
+++ frontend/src/BusinessLogic/usePlayer.js
@@ -3,13 +3,27 @@ import axios from "axios";
 import API from "./API";
 
 export function usePlayer() {
-    const [playerData, setPlayerData] = useState([]);
+    const [playerData, setPlayerData] = useState({});
 
     useEffect(() => {
         // Get player data from API
         // TODO: Handle api response properly
         axios.get(`${API.url}players/1`)
-            .then(res => setPlayerData(res.data))
+            .then(res => setPlayerData({
+                url: res.data.url,
+                firstName: res.data.firstName,
+                lastName: res.data.lastName,
+                alias: res.data.alias,
+                age: res.data.age,
+                gender: res.data.gender,
+                nationality: res.data.nationality,
+                gameSense: res.data.gameSense,
+                teamFighting: res.data.teamFighting,
+                dueling: res.data.dueling,
+                jglPathing: res.data.jglPathing,
+                waveMgmt: res.data.waveMgmt,
+                farming: res.data.farming
+            }))
             .catch(err => console.error(err));
         console.log(playerData);
     }, []);
