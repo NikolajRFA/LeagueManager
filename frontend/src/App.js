f4b210546d2629bfd0829b5bdcf1cd25309015aa
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-01-31 21:52:04 +0100

    Added Dashboard template page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Pages/Home";
 import Player from "./Pages/Player";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/player" element={<Player/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/Pages/Player.jsx
+++ frontend/src/Pages/Player.jsx
@@ -9,22 +9,25 @@ export default function Player() {
     document.body.style.backgroundColor = '#d9e6ec';
 
     return (
-        <Loading>
-            <Card>
-                <Box sx={{p: 2, display: 'flex'}}>
-                    <Avatar variant='rounded' sx={{ width: 50, height: 50, marginRight: '10px'}}>
-                        <PersonIcon/>
-                    </Avatar>
-                    <Stack spacing={0.5}>
-                        <Typography fontWeight="bold">
-                            {playerData.firstName} {playerData.lastName}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {playerData.alias}
-                        </Typography>
-                    </Stack>
-                </Box>
-            </Card>
-        </Loading>
+        <>
+
+            <Loading>
+                <Card>
+                    <Box sx={{p: 2, display: 'flex'}}>
+                        <Avatar variant='rounded' sx={{width: 50, height: 50, marginRight: '10px'}}>
+                            <PersonIcon/>
+                        </Avatar>
+                        <Stack spacing={0.5}>
+                            <Typography fontWeight="bold">
+                                {playerData.firstName} {playerData.lastName}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {playerData.alias}
+                            </Typography>
+                        </Stack>
+                    </Box>
+                </Card>
+            </Loading>
+        </>
     )
 }
