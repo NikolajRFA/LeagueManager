cf4846759c8aa651c0e3d8fa29a43d09e557e7fc
NikolajRFA <nrfadk@gmail.com>
2024-02-22 20:42:57 +0100

    Implemented Player class as data model

--- frontend/src/BusinessLogic/Models/Player.js
+++ frontend/src/BusinessLogic/Models/Player.js
@@ -2,25 +2,23 @@ import ApiReturn from "./ApiReturn";
 
 export default class Player extends ApiReturn{
     constructor(data = null) {
-        console.log(data);
         super();
         if (data == null) return;
-        console.log('Building player');
-        const url = data.url;
-        const firstName = data.firstName;
-        const lastName = data.lastName;
-        const alias = data.alias;
-        const age = data.age;
-        const gender = data.gender;
-        const nationality = data.nationality;
-        const gameSense = data.gameSense;
-        const teamFighting = data.teamFighting;
-        const dueling = data.dueling;
-        const jglPathing = data.jglPathing;
-        const waveMgmt = data.waveMgmt;
-        const farming = data.farming;
-        const activeTeamUrl = data.activeTeamUrl;
-        const activeTeam = data.activeTeam;
+        this.url = data.url;
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.alias = data.alias;
+        this.age = data.age;
+        this.gender = data.gender;
+        this.nationality = data.nationality;
+        this.gameSense = data.gameSense;
+        this.teamFighting = data.teamFighting;
+        this.dueling = data.dueling;
+        this.jglPathing = data.jglPathing;
+        this.waveMgmt = data.waveMgmt;
+        this.farming = data.farming;
+        this.activeTeamUrl = data.activeTeamUrl;
+        this.activeTeam = data.activeTeam;
 
         this.loading = true;
     }
--- frontend/src/BusinessLogic/usePlayer.js
+++ frontend/src/BusinessLogic/usePlayer.js
@@ -1,33 +1,17 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 import API from "./API";
+import Player from "./Models/Player";
 
 export function usePlayer(id) {
-    const [playerData, setPlayerData] = useState({});
+    const [playerData, setPlayerData] = useState(new Player());
 
     useEffect(() => {
         // Get player data from API
         axios.get(`${API.url}players/${id}`)
-            .then(res => setPlayerData({
-                url: res.data.url,
-                firstName: res.data.firstName,
-                lastName: res.data.lastName,
-                alias: res.data.alias,
-                age: res.data.age,
-                gender: res.data.gender,
-                nationality: res.data.nationality,
-                gameSense: res.data.gameSense,
-                teamFighting: res.data.teamFighting,
-                dueling: res.data.dueling,
-                jglPathing: res.data.jglPathing,
-                waveMgmt: res.data.waveMgmt,
-                farming: res.data.farming,
-                currentTeamUrl: res.data.currentTeamUrl,
-                currentTeam: res.data.currentTeam
-            }))
+            .then(res => setPlayerData(new Player(res.data)))
             .catch(err => console.error(err));
     }, []);
 
-    console.log(playerData);
     return playerData;
 }
