c2ca1d37972913fa4240c51226606bf113d1a6f8
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-03-20 22:55:10 +0100

    Created AddMember method

---
+++ DataLayer/DataServices/MemberDataService.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Entities;
+
+namespace DataLayer.DataServices;
+
+public class MemberDataService
+{
+    public bool AddMember(int teamId, int playerId, DateOnly fromDate)
+    {
+        var db = new Database();
+        db.Members.Add(new Member
+        {
+            TeamId = teamId,
+            PlayerId = playerId,
+            FromDate = fromDate,
+            ToDate = null
+        });
+        return db.SaveChanges() > 0;
+    }
+}
