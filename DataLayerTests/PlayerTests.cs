8b15272d1c1ac5e16a3a62f54fef68fb1d858eeb
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-03-24 20:40:33 +0100

    Added tests for the MemberDataService

--- DataLayerTests/PlayerTests.cs
+++ DataLayerTests/PlayerTests.cs
@@ -54,4 +54,25 @@ public class PlayerTests
         var memberDataService = new MemberDataService();
         Assert.True(memberDataService.IsMember(5));
     }
+
+    [Fact]
+    public void TerminateMember_Player7Team1_False()
+    {
+        var memberDataService = new MemberDataService();
+        Assert.False(memberDataService.TerminateMember(1, 7, DateOnly.FromDateTime(DateTime.Now)));
+    }
+    
+    [Fact]
+    public void TerminateMember_Player7Team2_True()
+    {
+        var memberDataService = new MemberDataService();
+        Assert.True(memberDataService.TerminateMember(2, 7, DateOnly.FromDateTime(DateTime.Now)));
+    }
+
+    [Fact]
+    public void AddMember_Player7Team2_True()
+    {
+        var memberDataService = new MemberDataService();
+        Assert.True(memberDataService.AddMember(2, 7, DateOnly.FromDateTime(DateTime.Now.AddDays(1))));
+    }
 }
