0166bab488c405206dcac82db277a6d4b805fca9
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2024-04-21 20:25:55 +0200

    Started making SeriesSingleSeries

--- frontend/src/Pages/Series/SeriesSingleSeries.tsx
+++ frontend/src/Pages/Series/SeriesSingleSeries.tsx
@@ -1,14 +1,35 @@
 import React, {FC} from "react";
 import {Card} from "@mui/material";
 import Series from "../../BusinessLogic/Models/Series";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 
 interface GamesGameProps {
     game: Series;
 }
 
 const SeriesSingleSeries: FC<GamesGameProps> = ({game}) => {
+    const center = {display: 'flex', alignItems: 'center', justifyContent: 'center'}
+    const blueSideIsWinner = game.blueSideUrl === game.winnerUrl;
+
     return (
-        <Card style={{height: '90px'}}>{game.blueSide} vs. {game.redSide}</Card>
+        <Card style={{height: '90px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <Grid container style={center}>
+                <Grid item lg={4} style={center}>
+                    <Paper style={{backgroundColor: blueSideIsWinner ? '#90ee90' : '#FFC0CB'}}>
+                        {game.blueSide}
+                    </Paper>
+                </Grid>
+                <Grid item lg={4} style={center}>
+                    vs.
+                </Grid>
+                <Grid item lg={4} style={center}>
+                    <Paper style={{backgroundColor: !blueSideIsWinner ? '#90ee90' : '#FFC0CB'}}>
+                        {game.redSide}
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Card>
     )
 }
 
